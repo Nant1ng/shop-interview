25ccf7b07e8b4ebb15c67ac49fd1618fd500092e
Andrés <andressantana99@hotmail.se>
2022-11-13 00:19:16 +0100

    styled components

---
+++ pages/shop/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import styled from 'styled-components';
+
+const Tndjf = styled.h1``;
+
+function Shop() {
+  return (
+    <Tndjf>asf</Tndjf>
+  );
+}
+
+export default Shop;
