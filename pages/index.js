00dc6e669dba0ecb46b22c45af8c9ddb1bc059fe
Andrés <andressantana99@hotmail.se>
2022-11-13 00:19:48 +0100

    shop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -16,30 +16,40 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by reading the{' '}
-          <code className={styles.code}>README.md</code>{' '}file
+          Get started by reading the{" "}
+          <code className={styles.code}>README.md</code> file
         </p>
 
         <div className={styles.grid}>
-          <a href="https://github.com/Black-Pixel-AB/shop-interview" target="_blank" rel="noreferrer" className={styles.card}>
+          <a
+            href="https://github.com/Black-Pixel-AB/shop-interview"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.card}
+          >
             <h2>Instructions &rarr;</h2>
             <p>Check out the instructions for the project!</p>
           </a>
 
-
           <a
             href="https://www.figma.com/file/z7jZJGxVyjScHaNlVVlh6d/Shop-Interview?node-id=0%3A1"
-            target="_blank" 
+            target="_blank"
             rel="noreferrer"
             className={styles.card}
           >
             <h2>Design &rarr;</h2>
-            <p>
-              The store is based on the attached design
-            </p>
+            <p>The store is based on the attached design</p>
+          </a>
+
+          <a
+            href="http://localhost:3000/shop"
+            className={styles.card}
+          >
+            <h2>Shop &rarr;</h2>
+            <p>The Shop </p>
           </a>
         </div>
       </main>
     </div>
-  )
+  );
 }
